b55faa7b93b2a87f96ced9ecbd85f5371c5d0ffa
bienao666 <1319174340@qq.com>
2022-02-09 15:24:23 +0800

    开卡更新

--- kaika/jd_opencardL62.js
+++ kaika/jd_opencardL62.js
@@ -55,7 +55,7 @@ let activityCookie =''
   $.userId = '10299171'
   $.actId = '29c257bced_220124'
   $.MixNicks = ''
-  $.inviteNick = ''
+  $.inviteNick = 'Hlciw9EWHlDhoUuC/DLAC14tLNYA4seuA67MOIYQxEk3Vl9+AVo4NF+tgyeIc6A6kdK3rLBQpEQH9V4tdrrh0w=='
   console.log(`活动地址:https://jinggengjcq-isv.isvjcloud.com/fronth5/#/pages/unitedCardNew20220124/unitedCardNew20220124?actId=29c257bced_220124`)
   console.log(`请自行测试有水无水。`)
   for (let i = 0; i < cookiesArr.length; i++) {
