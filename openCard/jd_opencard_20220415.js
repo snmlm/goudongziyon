3bc4382e668be1e218037169be83d69bf75e11c7
ssh1 <1319174340@qq.com>
2022-04-15 13:29:52 +0800

    开卡更新

--- openCard/jd_opencard_20220415.js
+++ openCard/jd_opencard_20220415.js
@@ -45,6 +45,11 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 CryptoScripts()
 $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
 let cleanCart = ''
+let isKaiKa= $.isNode() ? (process.env.isKaiKa ? process.env.isKaiKa : "") : ($.getdata('isKaiKa') ? $.getdata('isKaiKa') : "")
+  if (isKaiKa!="true") {
+    console.log("默认不执行开卡脚本，有水时请先去环境变量配置isKaiKa，值为true，没水时请禁用这个环境变量")
+    return;
+  }
 if($.isNode()){
   try{
     const fs = require('fs');
