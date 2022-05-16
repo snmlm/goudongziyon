82cb7420700e700a97bbffc63cc90d2630f8d9d4
ssh1 <1319174340@qq.com>
2022-05-16 20:51:11 +0800

    开卡更新

--- openCard/jd_lzdz1_customized5_16_2.js
+++ openCard/jd_lzdz1_customized5_16_2.js
@@ -56,7 +56,7 @@ if ($.isNode()) {
       $.ADID = getUUID("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", 1);
       $.UUID = getUUID("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
       authorCodeList = [
-        ''];
+        'fca71ebd28dc4042a3b5cba269de9658'];
       // $.authorCode = authorCodeList[random(0, authorCodeList.length)];
       $.authorCode = ownCode ? ownCode : authorCodeList[random(0, authorCodeList.length)]
       $.authorNum = `${random(1000000, 9999999)}`;
