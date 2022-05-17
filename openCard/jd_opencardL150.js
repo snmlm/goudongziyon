4cd1be1060d9667ab27152623722e685e9251149
ssh1 <1319174340@qq.com>
2022-05-18 00:27:42 +0800

    开卡更新

--- openCard/jd_opencardL150.js
+++ openCard/jd_opencardL150.js
@@ -22,7 +22,11 @@ cron:29 0 18-31 5 *
 const $ = new Env('5.18-5.31 五月宠爱季 和你在一起')
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
-
+let isKaiKa= $.isNode() ? (process.env.isKaiKa ? process.env.isKaiKa : "") : ($.getdata('isKaiKa') ? $.getdata('isKaiKa') : "")
+if (isKaiKa!="true") {
+    console.log("默认不执行开卡脚本，有水时请先去环境变量配置isKaiKa，值为true，没水时请禁用这个环境变量")
+    return;
+}
 //IOS等用户直接用NobyDa的jd cookie
 
 let cookiesArr = [],
