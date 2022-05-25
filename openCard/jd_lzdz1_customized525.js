ef882bee9a949f4d788069e0c2c8a952aea4f637
dwtian <dwtian@sedwt.com.cn>
2022-05-25 14:03:51 +0800

    开卡更新

--- openCard/jd_lzdz1_customized525.js
+++ openCard/jd_lzdz1_customized525.js
@@ -4,6 +4,11 @@
 const $ = new Env("快乐自定义 宅家云出游");
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
+let isKaiKa= $.isNode() ? (process.env.isKaiKa ? process.env.isKaiKa : "") : ($.getdata('isKaiKa') ? $.getdata('isKaiKa') : "")
+if (isKaiKa!="true") {
+    console.log("默认不执行开卡脚本，有水时请先去环境变量配置isKaiKa，值为true，没水时请禁用这个环境变量")
+    return;
+}
 let cookiesArr = [], cookie = '', message = '';
 let ownCode = null;
 if ($.isNode()) {
