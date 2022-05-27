0d3d10ddaa880d8214a782296ac126c1627199f8
dwtian <dwtian@sedwt.com.cn>
2022-05-27 14:27:30 +0800

    5.27-5.30 品类联合

--- openCard/jd_opencardL161.js
+++ openCard/jd_opencardL161.js
@@ -21,7 +21,11 @@ const $ = new Env('5.27-5.30 品类联合');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 //IOS等用户直接用NobyDa的jd cookie
-
+let isKaiKa= $.isNode() ? (process.env.isKaiKa ? process.env.isKaiKa : "") : ($.getdata('isKaiKa') ? $.getdata('isKaiKa') : "")
+if (isKaiKa!="true") {
+  console.log("默认不执行开卡脚本，有水时请先去环境变量配置isKaiKa，值为true，没水时请禁用这个环境变量")
+  return;
+}
 let cookiesArr = [],
     cookie = '';
 if ($.isNode()) {
