16abe5f808c48241fbe3b8ba6caa8ed98e1f29e8
ssh1 <1319174340@qq.com>
2022-07-22 01:07:04 +0800

    超燃好物 七夕献礼

--- openCard/jd_lzdz1_customized6.js
+++ openCard/jd_lzdz1_customized6.js
@@ -4,6 +4,11 @@
 
 */
 const $ = new Env("超燃好物 七夕献礼");
+let isKaiKa= $.isNode() ? (process.env.isKaiKa ? process.env.isKaiKa : "") : ($.getdata('isKaiKa') ? $.getdata('isKaiKa') : "")
+if (isKaiKa!="true") {
+    console.log("默认不执行开卡脚本，有水时请先去环境变量配置isKaiKa，值为true，没水时请禁用这个环境变量")
+    return;
+}
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', message = '';
