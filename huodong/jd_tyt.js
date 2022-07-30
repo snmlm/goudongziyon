9e69d35fce39052a58e02175a84137ff4f2d047c
ssh1 <1319174340@qq.com>
2022-07-30 19:42:53 +0800

    极速版-推推赚大钱

--- huodong/jd_tyt.js
+++ huodong/jd_tyt.js
@@ -1,6 +1,6 @@
 /*
 注意：助力码每天会变，旧的不可用。
-助力逻辑：优先助力互助码变量，默认助力第一个可助力的账号，需要修改助力人数修改代码57行的数字即可
+助力逻辑：优先助力互助码变量，默认助力前三个可助力的账号，需要修改助力人数修改代码57行的数字即可
 入口-极速版-推推赚大钱  5元无门槛卷 大概需要50人助力
  [task_local]
 #快速推一推
@@ -38,7 +38,7 @@ if ($.isNode()) {
                $.isLogin = true;
                $.nickName = '';
                message = '';
-               await TotalBean();
+               //await TotalBean();
                console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
                if (!$.isLogin) {
                     $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
@@ -53,8 +53,8 @@ if ($.isNode()) {
           await info()
           await coinDozerBackFlow()
           await getCoinDozerInfo()
-          console.log('\n注意助力第一个可助力的账号\n');
-          if (inviteCodes.length >= 1) {
+          console.log('\n注意助力前三个可助力的账号\n');
+          if (inviteCodes.length >= 3) {
                break
           }
      }
