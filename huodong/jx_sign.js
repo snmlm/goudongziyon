3d0f352425a87aaac228614f510d7d70bc1495f3
ssh1 <1319174340@qq.com>
2022-07-30 19:49:54 +0800

    京喜签到

--- huodong/jx_sign.js
+++ huodong/jx_sign.js
@@ -62,10 +62,10 @@ if ($.isNode()) {
       UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
       UAInfo[$.UserName] = UA
       if (isLoginInfo[$.UserName] === false) {
-      
+
       } else {
         if (!isLoginInfo[$.UserName]) {
-          await TotalBean();
+          //await TotalBean();
           isLoginInfo[$.UserName] = $.isLogin
         }
       }
@@ -89,10 +89,10 @@ if ($.isNode()) {
       $.nickName = '';
       message = '';
       if (isLoginInfo[$.UserName] === false) {
-      
+
       } else {
         if (!isLoginInfo[$.UserName]) {
-          await TotalBean();
+          //await TotalBean();
           isLoginInfo[$.UserName] = $.isLogin
         }
       }
@@ -128,12 +128,12 @@ if ($.isNode()) {
     }
   }
 })()
-  .catch((e) => {
-    $.log("", `❌ ${$.name}, 失败! 原因: ${e}!`, "");
-  })
-  .finally(() => {
-    $.done();
-  })
+    .catch((e) => {
+      $.log("", `❌ ${$.name}, 失败! 原因: ${e}!`, "");
+    })
+    .finally(() => {
+      $.done();
+    })
 
 async function main(help = true) {
   $.commonlist = []
@@ -340,27 +340,27 @@ function dotask(task) {
   }
   return new Promise((resolve) => {
     $.get(taskUrl(functionId, body), async (err, resp, data) => {
-        try {
-          if (err) {
-            console.log(JSON.stringify(err));
-            console.log(`${$.name} dotask API请求失败，请检查网路重试`);
+      try {
+        if (err) {
+          console.log(JSON.stringify(err));
+          console.log(`${$.name} dotask API请求失败，请检查网路重试`);
+        } else {
+          data = JSON.parse(data.match(new RegExp(/jsonpCBK.?\((.*);*/))[1])
+          if (data.ret === 0) {
+            console.log($.signhb_source === '5' ? `完成任务 获得${data.sendxd}喜豆` : `完成任务 获得${data.sendhb}红包`);
+          } else if (data.ret === 1003) {
+            console.log(`此账号已黑`);
+            $.black = true;
           } else {
-            data = JSON.parse(data.match(new RegExp(/jsonpCBK.?\((.*);*/))[1])
-            if (data.ret === 0) {
-              console.log($.signhb_source === '5' ? `完成任务 获得${data.sendxd}喜豆` : `完成任务 获得${data.sendhb}红包`);
-            } else if (data.ret === 1003) {
-              console.log(`此账号已黑`);
-              $.black = true;
-            } else {
-              console.log(JSON.stringify(data));
-            }
+            console.log(JSON.stringify(data));
           }
-        } catch (e) {
-          $.logErr(e, resp);
-        } finally {
-          resolve(data);
         }
-      });
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve(data);
+      }
+    });
   });
 }
 
@@ -472,7 +472,7 @@ function taskUrl(functionId, body = '') {
       "User-Agent": UA,
       "Accept-Language": "zh-CN,zh-Hans;q=0.9",
       "Referer": "https://st.jingxi.com/",
-      "Cookie": cookie
+      "Cookie": `cid=4;${cookie}`
     }
   }
 }
@@ -488,7 +488,7 @@ function JDtaskUrl(functionId, body = '') {
       "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
       "Accept-Language": "zh-CN,zh-Hans;q=0.9",
       "Referer": "https://wqs.jd.com/",
-      "Cookie": cookie
+      "Cookie": `cid=4;${cookie}`
     }
   }
 }
