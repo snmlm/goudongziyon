6c97bf7358cfef72352156fe0354ee878a3e1dd8
bienao666 <1319174340@qq.com>
2023-04-01 20:46:39 +0800

    汪汪赛跑提现

--- huodong/jd_joy_run_reward.ts
+++ huodong/jd_joy_run_reward.ts
@@ -1,13 +1,12 @@
 /**
-汪汪赛跑-提现10元
-2 0 0 * * 5 jd_joy_run_reward.ts
-new Env('汪汪赛跑提现')
-Modify By Dylan from HW
-updateTime：2022-07-09
-**/
+ 汪汪赛跑-提现10元,周五9点
+ 59 59 8 * * 5 jd_joy_run_reward.ts
+ new Env('汪汪赛跑提现')
+ updateTime：2022-07-09
+ **/
 
-import {get, post, requireConfig, wait} from './function/TS_USER_AGENTS'
-import {H5ST} from "./function/h5st"
+import { get, post, requireConfig, wait } from './TS_USER_AGENTS'
+import { H5ST } from "./function/h5st"
 
 let cookie: string = '', res: any = '', UserName: string = '', fp_448de: string = '' || process.env.FP_448DE, fp_b6ac3: string = '' || process.env.FP_B6AC3
 let h5stTool: H5ST = null
@@ -22,19 +21,22 @@ let h5stTool: H5ST = null
     try {
       h5stTool = new H5ST('448de', 'jdltapp;', fp_448de)
       await h5stTool.__genAlgo()
-      res = await team('runningMyPrize', {"linkId": "L-sOanK_5RJCz7I314FpnQ", "pageSize": 20, "time": null, "ids": null})
-	  rewardAmount = res.data.rewardAmount
+      res = await team('runningMyPrize', { "linkId": "L-sOanK_5RJCz7I314FpnQ", "pageSize": 20, "time": null, "ids": null })
+      rewardAmount = res.data.rewardAmount
       if (res.data.runningCashStatus.currentEndTime) {
         console.log('可提现', rewardAmount)
-        res = await api('runningPrizeDraw', {"linkId": "L-sOanK_5RJCz7I314FpnQ", "type": 2, "level": 3})
-        await wait(2000)
-        if (res.success){
-               console.log(res.data.message)
-           } else {
-                console.log('提现失败：', res.errMsg)
-             }
-      }else{
-          console.log('还未到提现时间')
+        res = await api('runningPrizeDraw', { "linkId": "L-sOanK_5RJCz7I314FpnQ", "type": 2, "level": 3 })
+        if (res.errMsg.indexOf("不足") > -1) {
+          res = await api('runningPrizeDraw', { "linkId": "L-sOanK_5RJCz7I314FpnQ", "type": 2, "level": 2 })
+        }
+        await wait(1000)
+        if (res.success) {
+          console.log(res.data.message)
+        } else {
+          console.log('提现失败：', res.errMsg)
+        }
+      } else {
+        console.log('还未到提现时间')
       }
     } catch (e) {
       console.log('Error', e)
